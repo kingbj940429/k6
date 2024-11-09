32bd920180d0c3d16fa27ff4388933f216abc263
kingbj0429 <kingbj0429@lunit.io>
2024-11-09 18:03:16 +0900

    add: -

--- expressions/boilerplate-complete-03.js
+++ expressions/boilerplate-complete-03.js
@@ -6,58 +6,76 @@ import exec from 'k6/execution';
 
 const httpErrors = new Counter('http_errors');
 const requestCounter = new Counter('request_counter');
-const baseUrl = "https://command-center-server.dev.insight.lunit.io";
+const baseUrl = "https://command-center-server.blue.lunit.in";
+const endpoints = {
+    health: `${baseUrl}/health`,
+    apps: {
+        list: `${baseUrl}/api/v1/apps`,
+    }
+}
+
+const groups = {
+    health_check: 'health_check',
+    apps: {
+        name: 'apps',
+        list: 'list_apps',
+    }
+};
 
 export const options = {
     discardResponseBodies: false,
     tags: {
-        testid: 'command-center-dev-load'
+        testid: 'command-center-prod-blue-short-load'
     },
-    thresholds: {
-        http_req_duration: ['p(95)<500'],
-        'http_req_duration{status:200}': ['p(95)>5'],
-        'http_req_duration{status:400}': ['p(95)<5'],
+    thresholds: (() => {
+        const thresholds = {};
 
-        http_errors: ['count==0'],
-        'http_errors{group_tag: health_check}': ['count==0'],
-        'http_errors{group_tag: list_apps}': ['count==0'],
+        thresholds[`http_req_duration`] = ['p(95)<250'];
+        thresholds[`http_req_duration{status:200}`] = ['p(95)>5'];
+        thresholds[`http_req_duration{status:400}`] = ['p(95)<5'];
 
-        request_counter: ['count>100'],
+        thresholds[`http_errors`] = ['count==0'];
+        thresholds[`http_errors{group_tag: ${groups.health_check}}`] = ['count==0'];
+        thresholds[`http_errors{group_tag: ${groups.apps.list}}`] = ['count==0'];
 
-        checks: ['rate>=0.99'],
-        'checks{group_tag: health_check}': ['rate>=0.99'],
-        'checks{group_tag: list_apps}': ['rate>=0.99'],
+        thresholds[`checks`] = ['rate>=0.99'];
+        thresholds[`checks{group_tag: ${groups.health_check}}`] = ['rate>=0.99'];
+        thresholds[`checks{group_tag: ${groups.apps.list}}`] = ['rate>=0.99'];
 
-        'group_duration{group:::Health Check}': ['p(95)<200'],
-        'group_duration{group:::Apps}': ['p(95)<200'],
-        'group_duration{group:::Apps::List Apps}': ['p(95)<200'],
-    },
+        thresholds[`group_duration{group:::${groups.health_check}}`] = ['p(95)<250'];
+        thresholds[`group_duration{group:::${groups.apps.name}}`] = ['p(95)<250'];
+        thresholds[`group_duration{group:::${groups.apps.name}::${groups.apps.list}}`] = ['p(95)<250'];
+
+        thresholds[`request_counter`] = ['count>50'];
+
+        return thresholds;
+    })(),
     scenarios: {
         smoke_test: {
             executor: 'per-vu-iterations',
             exec: 'smokeTest',
             vus: 1,
             iterations: 1,
-            maxDuration: '10s',
-            tags : {type: 'smoke'}
+            maxDuration: '3s',
+            tags: {type: 'smoke'}
         },
         load_test: {
             executor: 'ramping-vus',
             exec: 'loadTest',
             startVUs: 0,
-            startTime: '10s',
+            startTime: '3s',
             stages: [
-                {duration: '10s', target: 100},
-                {duration: '1m', target: 100},
-                {duration: '10s', target: 0},
+                {duration: '2s', target: 30},
+                {duration: '10s', target: 30},
+                {duration: '2s', target: 0},
             ],
-            tags : {type: 'load'}
+            tags: {type: 'load'}
         },
     },
 }
 
 export function setup() {
-    const res = http.get(`${baseUrl}/health`);
+    const res = http.get(endpoints.health);
     if (res.error) {
         exec.test.abort('Aborting test. Application is DOWN');
     }
@@ -75,18 +93,18 @@ export function teardown() {
 }
 
 export function smokeTest(accessToken) {
-    group('Health Check', function () {
-        let res = http.get(`${baseUrl}/health`, {
+    group(groups.health_check, function () {
+        let res = http.get(endpoints.health, {
             headers: {'Authorization': `Bearer ${accessToken}`},
-        }, {group_tag: "health_check"}); // status 200
+        }, {group_tag: groups.health_check}); // status 200
         if (res.error) {
-            httpErrors.add(1, {group_tag: "health_check"});
+            httpErrors.add(1, {group_tag: groups.health_check});
         }
 
         check(res, {
             'status is 200': (r) => r.status === 200,
             'status is not 400': (r) => r.status !== 400,
-        }, {group_tag: "health_check"});
+        }, {group_tag: groups.health_check});
 
         requestCounter.add(1);
     });
@@ -95,18 +113,18 @@ export function smokeTest(accessToken) {
 }
 
 export function loadTest(accessToken) {
-    group('Apps', function () {
-        group('List Apps', function () {
-            let res = http.get(`${baseUrl}/api/v1/apps`, {
+    group(groups.apps.name, function () {
+        group(groups.apps.list, function () {
+            let res = http.get(endpoints.apps.list, {
                 headers: {'Authorization': `Bearer ${accessToken}`},
-            }, {group_tag: "list_apps"});
+            }, {group_tag: groups.apps.list});
             if (res.error) {
-                httpErrors.add(1, {group_tag: "list_apps"});
+                httpErrors.add(1, {group_tag: groups.apps.list});
             }
 
             check(res, {
                 'status is 200': (r) => r.status === 200,
-            }, {group_tag: "list_apps"});
+            }, {group_tag: groups.apps.list});
 
             requestCounter.add(1);
         });
@@ -117,7 +135,7 @@ export function loadTest(accessToken) {
 
 function getAccessToken() {
     const data = {
-        client_id: 'command-center',
+        client_id: __ENV.CLIENT_ID || 'command-center',
         grant_type: 'password',
         scope: 'email openid profile roles',
         username: __ENV.TESTER_USERNAME || 'k6-tester@lunit.io',
