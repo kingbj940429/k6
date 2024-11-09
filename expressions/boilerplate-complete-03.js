20384352a960ae74204676066bf01b70cba1546c
kingbj0429 <kingbj0429@lunit.io>
2024-11-09 10:38:15 +0900

    add: -

---
+++ expressions/boilerplate-complete-03.js
@@ -0,0 +1,129 @@
+import http from 'k6/http';
+import {sleep, check, group} from 'k6';
+import {Counter} from 'k6/metrics';
+import {randomIntBetween} from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import exec from 'k6/execution';
+
+const httpErrors = new Counter('http_errors');
+const requestCounter = new Counter('request_counter');
+const baseUrl = "https://command-center-server.dev.insight.lunit.io";
+
+export const options = {
+    discardResponseBodies: false,
+    tags: {
+        testid: 'command-center-dev-load'
+    },
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+        'http_req_duration{status:200}': ['p(95)>5'],
+        'http_req_duration{status:400}': ['p(95)<5'],
+
+        http_errors: ['count==0'],
+        'http_errors{group_tag: health_check}': ['count==0'],
+        'http_errors{group_tag: list_apps}': ['count==0'],
+
+        request_counter: ['count>100'],
+
+        checks: ['rate>=0.99'],
+        'checks{group_tag: health_check}': ['rate>=0.99'],
+        'checks{group_tag: list_apps}': ['rate>=0.99'],
+
+        'group_duration{group:::Health Check}': ['p(95)<200'],
+        'group_duration{group:::Apps}': ['p(95)<200'],
+        'group_duration{group:::Apps::List Apps}': ['p(95)<200'],
+    },
+    scenarios: {
+        smoke_test: {
+            executor: 'per-vu-iterations',
+            exec: 'smokeTest',
+            vus: 1,
+            iterations: 1,
+            maxDuration: '10s',
+            tags : {type: 'smoke'}
+        },
+        load_test: {
+            executor: 'ramping-vus',
+            exec: 'loadTest',
+            startVUs: 0,
+            startTime: '10s',
+            stages: [
+                {duration: '10s', target: 100},
+                {duration: '1m', target: 100},
+                {duration: '10s', target: 0},
+            ],
+            tags : {type: 'load'}
+        },
+    },
+}
+
+export function setup() {
+    const res = http.get(`${baseUrl}/health`);
+    if (res.error) {
+        exec.test.abort('Aborting test. Application is DOWN');
+    }
+
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+        exec.test.abort('Aborting test. Unable to get access token');
+    }
+
+    return accessToken;
+}
+
+export function teardown() {
+    console.log('Tearing down...');
+}
+
+export function smokeTest(accessToken) {
+    group('Health Check', function () {
+        let res = http.get(`${baseUrl}/health`, {
+            headers: {'Authorization': `Bearer ${accessToken}`},
+        }, {group_tag: "health_check"}); // status 200
+        if (res.error) {
+            httpErrors.add(1, {group_tag: "health_check"});
+        }
+
+        check(res, {
+            'status is 200': (r) => r.status === 200,
+            'status is not 400': (r) => r.status !== 400,
+        }, {group_tag: "health_check"});
+
+        requestCounter.add(1);
+    });
+
+    sleep(randomIntBetween(1, 5));
+}
+
+export function loadTest(accessToken) {
+    group('Apps', function () {
+        group('List Apps', function () {
+            let res = http.get(`${baseUrl}/api/v1/apps`, {
+                headers: {'Authorization': `Bearer ${accessToken}`},
+            }, {group_tag: "list_apps"});
+            if (res.error) {
+                httpErrors.add(1, {group_tag: "list_apps"});
+            }
+
+            check(res, {
+                'status is 200': (r) => r.status === 200,
+            }, {group_tag: "list_apps"});
+
+            requestCounter.add(1);
+        });
+    });
+
+    sleep(randomIntBetween(1, 5));
+}
+
+function getAccessToken() {
+    const data = {
+        client_id: 'command-center',
+        grant_type: 'password',
+        scope: 'email openid profile roles',
+        username: __ENV.TESTER_USERNAME || 'k6-tester@lunit.io',
+        password: __ENV.TESTER_PASSWORD,
+    };
+    const res = http.post('https://keycloak.lunit.in/realms/lunit/protocol/openid-connect/token', data)
+
+    return res.json()['access_token'];
+}
